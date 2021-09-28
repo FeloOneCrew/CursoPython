ff0da5ed44764443f9a27d287f511f28a6808a87
FeloOneCrew <36139236+FeloOneCrew@users.noreply.github.com>
2021-09-28 16:53:29 -0500

    actualizacion de datos

--- datos.py
+++ datos.py
@@ -1,4 +1,9 @@
 print("Hola Mundo")
 Nombre ="Felipe Gómez"
 Edad= 33
+datos = Nombre, Edad
 print("El nombre es ", Nombre,"y su edad es ", Edad)
+print("Los datos serian: ", datos)
+Ciudad = str(input("¿De que ciudad eres? "))
+## print(f"El es de la ciudad de: {Ciudad}") esta es otro manera de imprimir
+print( "El es de la ciudad de: ", Ciudad)

10c984b136a6715ad39bfee11706897854768fad
FeloOneCrew <36139236+FeloOneCrew@users.noreply.github.com>
2021-09-29 15:08:14 -0500

    Continuacion del curso
    Primeros programas

---
+++ suma.py
@@ -0,0 +1,11 @@
+print("Suma de 2 Números enteros")
+
+##Entrada de datos
+n1 = int(input("Ingrese el primer número entero: "))
+n2 = int(input("Ingrese el segundo número entero: "))
+
+#Operacion
+suma= n1+n2 
+
+#Salida de datos
+print(f"La Suma de 2 Números enteros ingresados es:{suma}")

2f64c14ff524596af2cfc5d35cf5157ad950e71a
FeloOneCrew <36139236+FeloOneCrew@users.noreply.github.com>
2021-10-07 15:32:23 -0500

    Actualizacion

--- 03-Estructura de Control y colecciones/pilas.py
+++ 03-Estructura de Control y colecciones/pilas.py
@@ -5,12 +5,7 @@ una lista como una pila, donde el último elemento añadido
  es el primer elemento retirado («último en entrar, primero en salir»). Para agregar un elemento a la cima de la pila, utiliza append(). Para retirar un elemento de la cima de la pila, utiliza pop() sin un índice explícito.
 """
 
-print("Esto es una Pila")
-datos=[1, 2, 3, 4, 5 , 6 , 7, 8]
-print(datos)
-p=0
-y= int(len(datos))
-while p < y:
-    datos.pop()
-    print(datos)
-    p+=1
+
+
+
+

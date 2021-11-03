4e3338d641a04a25d4237eada4c17580b5042a31
FeloOneCrew <36139236+FeloOneCrew@users.noreply.github.com>
2021-11-02 19:04:26 -0500

    actualizacion

---
+++ 05- Modularidad/Entra_script.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv)==3:
+    texto= sys.argv[1]
+    cantidad = int(sys.argv[2])
+
+    c=0
+    while c < cantidad:
+        print(texto)
+        c+=1
+else:
+    print("Error, ingrese los argumentos correctamente")
+    print('Ejemplo: Entra_script "texto" 5')
+
